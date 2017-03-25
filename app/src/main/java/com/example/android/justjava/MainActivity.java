6dc4c5a95adbac79cca532bff0c93955bc59dff8
Yogesh Jariwala <yogesh.jariwala@riba.org>
2017-03-25 13:42:07 +0000

    Adding checkboxes

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,6 +7,7 @@
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 /**
@@ -26,8 +27,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        CheckBox CinnamonCheckBox = (CheckBox) findViewById(R.id.cinnamon_checkbox);
+        boolean hasCinnamon = CinnamonCheckBox.isChecked();
+        CheckBox ChocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = ChocolateCheckBox.isChecked();
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
+        String priceMessage = createOrderSummary(price, hasCinnamon, hasChocolate);
         displayMessage(priceMessage);
     }
 
@@ -40,8 +45,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private String createOrderSummary(int price) {
+    private String createOrderSummary(int price, boolean hasCinnamon, boolean hasChocolate) {
         String priceMessage = "Name: Bullet Club";
+        priceMessage = priceMessage + "\nAdd Cinnamon? " + hasCinnamon;
+        priceMessage = priceMessage + "\nAdd Chocolate? " + hasChocolate;
         priceMessage = priceMessage + "\nQuantity: " + quantity;
         priceMessage = priceMessage + "\nTotal: £" + (quantity * 5);
         priceMessage = priceMessage + "\nThank you!";
