50f01ce87146ed46567f16600f7f53e0c40b7ad0
Yogesh Jariwala <yogesh.jariwala@riba.org>
2017-03-27 21:35:24 +0100

    If Statements and other things

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,11 +4,15 @@
  *
  */ package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -27,42 +31,77 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText whatIsName = (EditText) findViewById(R.id.name_input);
+        String nameValue = whatIsName.getText().toString();
+
         CheckBox CinnamonCheckBox = (CheckBox) findViewById(R.id.cinnamon_checkbox);
         boolean hasCinnamon = CinnamonCheckBox.isChecked();
+
         CheckBox ChocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = ChocolateCheckBox.isChecked();
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasCinnamon, hasChocolate);
-        displayMessage(priceMessage);
+
+        int price = calculatePrice(hasCinnamon, hasChocolate);
+
+        String priceMessage = createOrderSummary(price, hasCinnamon, hasChocolate, nameValue);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order For " + nameValue);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
      * Calculates the price of the order.
      */
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean addCinnamon, boolean addChocolate) {
+        int basePrice = 5;
+
+        if (addCinnamon) {
+            basePrice = basePrice + 1;
+        }
+
+        if (addChocolate) {
+            basePrice = basePrice + 2;
+        }
+
+        return quantity * basePrice;
+
     }
 
 
-    private String createOrderSummary(int price, boolean hasCinnamon, boolean hasChocolate) {
-        String priceMessage = "Name: Bullet Club";
+    private String createOrderSummary(int basePrice, boolean hasCinnamon, boolean hasChocolate, String nameValue) {
+        String priceMessage = "Name: " + nameValue;
         priceMessage = priceMessage + "\nAdd Cinnamon? " + hasCinnamon;
         priceMessage = priceMessage + "\nAdd Chocolate? " + hasChocolate;
         priceMessage = priceMessage + "\nQuantity: " + quantity;
-        priceMessage = priceMessage + "\nTotal: £" + (quantity * 5);
+        priceMessage = priceMessage + "\nTotal: £" + (basePrice);
         priceMessage = priceMessage + "\nThank you!";
         return priceMessage;
     }
 
     public void increment(View view) {
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
+
     }
 
     public void decrement(View view) {
-        quantity = quantity - 1;
+        if (quantity > 0) {
+            quantity = quantity - 1;
+        } else {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+        }
         displayQuantity(quantity);
+
     }
 
     /**
@@ -75,11 +114,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView order_Summary_TextView = (TextView) findViewById(R.id.orderSummaryTextView);
-        order_Summary_TextView.setText(message);
-    }
 }
